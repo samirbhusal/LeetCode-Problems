2b451d2c9fe44cdcd3b46c13f31c7db6cb08aa24
Samir Bhusal <84609825+samirbhusal@users.noreply.github.com>
2026-09-12 13:29:05 -0500

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 704474-while-loop-GFG/704474-while-loop.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        // code here
+        
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
