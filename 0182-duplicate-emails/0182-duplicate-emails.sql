e4f9861f7cb8f4840602e7e28e65d3ae298ba956
Samir Bhusal <84609825+samirbhusal@users.noreply.github.com>
2026-09-23 12:13:17 -0500

    Time: 974 ms (5.02%), Space: 0B (100%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+Select email
+from Person
+Group By email
+having count(*) > 1;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
